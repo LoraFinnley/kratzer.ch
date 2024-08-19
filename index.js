825e4a8aa7ac2bf08fc2fbb41c4b863c727a227e
Linda Wyden <linda@LindasMac.local>
2024-08-19 19:12:24 +0200

    removed comments on index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-/*
-    - npm init -y
-    - npm i ejs express body-parser bootstrap@5.3.3
-    - package.json -> add the line "type": "module",
-
-    Add standard stuff:
-
-            import express from "express";
-            import bodyParser from "body-parser";
-            import { dirname } from "path";
-            import { fileURLToPath } from "url";
-
-            const app = express();
-            const port = 3000;
-
-            const __dirname = dirname(fileURLToPath(import.meta.url));
-            app.use(bodyParser.urlencoded({ extended: true }));
-
-            app.listen(port, () => {
-                console.log(`Server is running on port ${port}`);
-            });
-
-    nodemon index.js
-*/
-
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
